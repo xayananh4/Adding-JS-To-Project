5a50bfbaa0829b684f8ea1119a1e06f268d775d4
xayananh4 <xayananh4@gmail.com>
2023-01-09 07:44:38 -0800

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+saySomething("The sum is " + add(3,5) + ".");
+
