e93e8a7b25186c206746201d9880685aac5673e3
xayananh4 <xayananh4@gmail.com>
2023-01-09 10:23:06 -0800

    Calculate function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,49 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+// business logic
+let resultsForAdding = "";
 function add(number1, number2) {
   return number1 + number2;
+
 }
 
-saySomething("The sum is " + add(3,5) + ".");
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function allTheProblems(number1, number2) {
+  resultAdd = add(number1,number2);
+  resultSub = subtract(number1,number2);
+  resultDiv = multiply(number1, number2);
+  resultMulitply = multiply(number1,number2);
+  return "The addition of your numbers equals: " + resultAdd + "\n"  +
+  "The subtraction of your numbers equals: " + Math.abs(resultSub) + "\n"  +
+  "The division of your numbers equals: " + Math.abs(resultDiv) + "\n"  +
+  "The multiplication of your numbers equals: " + resultMulitply;
+}
+
+function allTheProblemsChallenging (number1, number2) {
+  resultAdd = add(number1,number2);
+  resultSub = subtract(number1,number2);
+  resultDiv = multiply(number1, number2);
+  resultMulitply = multiply(number1,number2);
+  return number1 + " + " + number2 + " = " + resultAdd + "\n"  +
+  number1 + " - " + number2 + " = "  + Math.abs(resultSub) + "\n"  +
+  number1 + " / " + number2 + " = "  + Math.abs(resultDiv) + "\n"  +
+  number1 + " * " + number2 + " = "  + resultMulitply;
+}
+
+
+// user interface logic 
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+
+alert(allTheProblemsChallenging(parseInt(number1), parseInt(number2)));
 
